692c5d3e899eb5f42f1f0c8a4828a9590b41778e
Dan Kenigsberg <danken@redhat.com>
2012-07-24 03:54:57 +0300

    pthreading initial commit
    Signed-off-by: Dan Kenigsberg <danken@redhat.com>

---
+++ setup.py
@@ -0,0 +1,35 @@
+from distutils.core import setup
+from distutils.cmd import Command
+from unittest import TextTestRunner, TestLoader
+
+import pthreading
+import tests
+
+
+class TestCommand(Command):
+    user_options = []
+    description = "run unit tests"
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        loader = TestLoader()
+        t = TextTestRunner()
+        t.run(loader.loadTestsFromModule(tests))
+
+setup(name='pthreading',
+      version='0.1.0',
+      license='GPLv2+',
+      url='http://git.fedorahosted.org/git/?p=pthreading.git',
+      maintainer='Dan Kenigsberg',
+      maintainer_email='danken@redhat.com',
+      py_modules=('pthreading', 'pthread'),
+      platforms=('Linux',),
+      description=pthreading.__doc__.split('\n')[0],
+      long_description=pthreading.__doc__,
+      cmdclass={'test': TestCommand},
+      )
